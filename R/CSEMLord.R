74e3f8365ecf7dbbc3d20ec26abb1edde7afdfea
liuhuan90123 <liuhuanbnu@gmail.com>
2020-02-04 22:51:41 -0600

    CSSEMKolen

--- R/CSEMLord.R
+++ R/CSEMLord.R
@@ -11,43 +11,41 @@ convTable <- read.csv("TestData/ConversionTableFormX.csv")
 
 convTable$roundedSS <- round(convTable$unroundedSS)
 
+convTableSub <- convTable[,c("rawScore", "roundedSS")]
 
-ss <- convTable[,c("rawScore", "roundedSS")]
-
-csem_ss <- matrix(nrow = 41, ncol = 2)
+csemDat <- matrix(nrow = 41, ncol = 2)
 
 for (i in 0:40){
 
-  # pi =20
   pi = i/40
 
-  bidat <- as.data.frame(c(0:40))
+  binoDat <- as.data.frame(c(0:40))
 
-  names(bidat) <- "raw"
+  names(binoDat) <- "raw"
 
-  bidat$pr <- choose(40, bidat$raw) * (pi)^bidat$raw * (1 - pi)^(40-bidat$raw)
+  binoDat$prob <- choose(40, binoDat$raw) * (pi)^binoDat$raw * (1 - pi)^(40-binoDat$raw)
 
-  bidat <- cbind(bidat, ss)
+  binoDat <- cbind(binoDat, convTableSub)
 
-  bidat$xpr <- bidat$rawScore * bidat$pr
-  bidat$x2pr <- bidat$rawScore^2 * bidat$pr
-  bidat$spr <- bidat$roundedSS * bidat$pr
-  bidat$s2pr <- bidat$roundedSS^2 * bidat$pr
+  binoDat$xprob <- binoDat$rawScore * binoDat$prob
+  binoDat$x2prob <- binoDat$rawScore^2 * binoDat$prob
+  binoDat$ssprob <- binoDat$roundedSS * binoDat$prob
+  binoDat$ss2prob <- binoDat$roundedSS^2 * binoDat$prob
 
-  sd_x <- sqrt(40/(40-1)) * sqrt(sum(bidat$x2pr) - sum(bidat$xpr)^2)
-  sd_s <- sqrt(40/(40-1)) * sqrt(sum(bidat$s2pr) - sum(bidat$spr)^2)
+  seX <- sqrt(40/(40-1)) * sqrt(sum(binoDat$x2prob) - sum(binoDat$xprob)^2)
+  seSS <- sqrt(40/(40-1)) * sqrt(sum(binoDat$ss2prob) - sum(binoDat$ssprob)^2)
 
-  csem_ss[i+1, 1] <- sd_x
-  csem_ss[i+1, 2] <- sd_s
+  csemDat[i+1, 1] <- seX
+  csemDat[i+1, 2] <- seSS
 
 }
 
 
-csem_ss <- as.data.frame(csem_ss)
+csemDat <- as.data.frame(csemDat)
 
-names(csem_ss) <- c("csemLord", "csemBinomial")
+names(csemDat) <- c("csemLord", "cssemBinomial")
 
-csem_ss$rawScore <- c(0:40)
+csemDat$rawScore <- c(0:40)
 
 # calculate raw score frequence
 
@@ -58,17 +56,15 @@ names(rawFreq) <- c("rawScore", "freq")
 
 convTable <- merge(convTable, rawFreq, by = "rawScore")
 convTable$wt <- convTable$freq / sum(convTable$freq)
-convTable <- merge(convTable, csem_ss, by = "rawScore")
+convTable <- merge(convTable, csemDat, by = "rawScore")
 
 
 
 # raw scale reliability using Lord CSEM
-
 LordRel <- 1 - sum(convTable$csemLord^2 * convTable$wt)/wt.var(convTable$rawScore, convTable$wt)
 
 # Scale score reliability using binomial method
-
-SSRelCTTBinomial <- 1 - sum(convTable$csemBinomial^2 * convTable$wt)/wt.var(convTable$roundedSS, convTable$wt)
+SSRelCTTBinomial <- 1 - sum(convTable$cssemBinomial^2 * convTable$wt)/wt.var(convTable$roundedSS, convTable$wt)
 
 
 # plot for Lord CSEM
@@ -86,16 +82,16 @@ dev.off()
 
 ## calculate average CSSEM: average of variance, not standard error
 
-convTable$varBinomial <- convTable$csemBinomial^2
+convTable$varBinomial <- convTable$cssemBinomial^2
 convTableBinomial <- aggregate(convTable$varBinomial, by=list(Category=convTable$roundedSS), FUN=mean)
 names(convTableBinomial) <- c("roundedSS", "varBinomialAvg")
-convTableBinomial$csemBinomialAvg <- sqrt(convTableBinomial$varBinomialAvg)
+convTableBinomial$cssemBinomialAvg <- sqrt(convTableBinomial$varBinomialAvg)
 
 
 # plot for Binomial CSSEM
 png("CSSEMCTTBinomial.png",  width = 799, height = 596)
 
-CSSEMCTTBinomialpng <- ggplot(convTableBinomial, aes(x = roundedSS, y = csemBinomialAvg)) +
+CSSEMCTTBinomialpng <- ggplot(convTableBinomial, aes(x = roundedSS, y = cssemBinomialAvg)) +
   geom_point() +
   scale_x_continuous(name = "Rounded Scale Score", breaks  = seq(100, 130, 5)) +
   scale_y_continuous(name = "CSSEM Binomial Method") +
@@ -108,7 +104,7 @@ dev.off()
 
 ## polynomial method
 
-m1 <- lm(Rounded.SS ~ 1 + RawScore + I(RawScore^2) + I(RawScore^3), CSEM_raw_uniq_ord)
+m1 <- lm(roundedSS ~ 1 + rawScore + I(rawScore^2) + I(rawScore^3), convTable)
 summary(m1)
 
 # first derivative
@@ -116,21 +112,32 @@ f <- expression(8.842e-04 * x^3 -5.671e-02* x^2 + 1.618e+00 * x + 9.899e+01)
 D(f, "x")
 
 # apply formula
-CSEM_raw_uniq_ord$fx <- 0.0008842 * (3 * CSEM_raw_uniq_ord$RawScore^2) - 0.05671 * (2 * CSEM_raw_uniq_ord$RawScore) + 1.618
-CSEM_raw_uniq_ord$CSSEM_poly <- CSEM_raw_uniq_ord$fx * CSEM_raw_uniq_ord$CSEM_Lord
+convTable$fx <- 0.0008842 * (3 * convTable$rawScore^2) - 0.05671 * (2 * convTable$rawScore) + 1.618
+convTable$cssemPoly <- convTable$fx * convTable$csemLord
 
 
 ### Scale score reliability with polynomial method
-CTT_SS_Reliability_poly_X <- 1 - sum(CSEM_raw_uniq_ord$CSSEM_poly^2 * CSEM_raw_uniq_ord$wt)/wt.var(CSEM_raw_uniq_ord$Rounded.SS, CSEM_raw_uniq_ord$wt)
+SSRelCTTPoly <- 1 - sum(convTable$cssemPoly^2 * convTable$wt)/wt.var(convTable$roundedSS, convTable$wt)
 
 
 
+## calculate average CSSEM: average of variance, not standard error
 
-CSEMLord <- function(rawData, ){
+convTable$varPoly <- convTable$cssemPoly^2
+convTablePoly <- aggregate(convTable$varPoly, by=list(Category=convTable$roundedSS), FUN=mean)
+names(convTablePoly) <- c("roundedSS", "varPolyAvg")
+convTablePoly$cssemPolyAvg <- sqrt(convTablePoly$varPolyAvg)
 
 
+# plot for Poly CSSEM
+png("CSSEMCTTPoly.png",  width = 799, height = 596)
 
-}
+CSSEMCTTPolypng <- ggplot(convTablePoly, aes(x = roundedSS, y = cssemPolyAvg)) +
+  geom_point() +
+  scale_x_continuous(name = "Rounded Scale Score", breaks  = seq(100, 130, 5)) +
+  scale_y_continuous(name = "CSSEM Poly Method") +
+  theme_bw()
 
-
+print(CSSEMCTTPolypng)
+dev.off()
 
