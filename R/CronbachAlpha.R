5893f86ebae67486734afb44286bb1147c9c2a11
liuhuan90123 <liuhuanbnu@gmail.com>
2020-02-06 13:51:27 -0600

    minor

--- R/CronbachAlpha.R
+++ R/CronbachAlpha.R
@@ -3,12 +3,16 @@
 
 
 CronbachAlpha <- function(dat){
+
   # num of culomns
   numColumn <- ncol(dat)
+
   # sum of each variable variance
   varianceItemSum <- sum(diag(cov(dat)))
+
   # sum of total variance
   varianceTotal <- sum(cov(dat))
+
   cronbachAlpha <- numColumn / (numColumn - 1) * (varianceTotal - varianceItemSum)/varianceTotal
   cronbachAlpha
 
--- R/Feldt.R
+++ R/Feldt.R
@@ -2,12 +2,16 @@
 #' @export
 
 Feldt <- function(dat){
+
   # sum of total variance
   varianceTotal <- sum(cov(dat))
+
   # sum of each variable variance
   varianceItemSum <- sum(diag(cov(dat)))
+
   # sum of covariance
   covarianceSum <- sum(rowSums(cov(dat))^2)
+
   feldt <- varianceTotal^2 / (varianceTotal^2 - covarianceSum) * (varianceTotal - varianceItemSum) / varianceTotal
   feldt
 
