52df0276e99d4a5f60170812b34ed770de471529
liuhuan90123 <liuhuanbnu@gmail.com>
2020-03-05 17:17:08 -0600

    info

--- R/Info.R
+++ R/Info.R
@@ -4,7 +4,7 @@
 #' A function to calculate information for 2PL model
 #'
 #' @param theta ability theta vector
-#' @param itemPara a text file with parameters of sequence b and a, a is on the 1.702 metric
+#' @param itemPara a text file with parameters of sequence b and a and c, a is on the 1.702 metric
 #' @param estType estimation method, MLE or EAP
 #'
 #' @return a data frame containing info
@@ -17,19 +17,46 @@
 
 Info <- function(theta, itemPara, estType){
 
-  #  item parameters should be on the 1.702 metric
-  names(itemPara) <- c("b", "a")
+  # itemPara <- itemPara_A # test
+  # theta <- NormalQuadraPoints(41)$nodes
+
+  if (ncol(itemPara) == 3){
+    #  item parameters should be on the 1.702 metric
+    names(itemPara) <- c("b", "a", "c")
+  }
+
+  if (ncol(itemPara) == 2){
+    #  item parameters should be on the 1.702 metric
+    names(itemPara) <- c("b", "a")
+    itemPara$c <- 0
+  }
+
+  if (ncol(itemPara) == 1){
+    #  item parameters should be on the 1.702 metric
+    names(itemPara) <- c("b")
+    itemPara$a <- 1
+    itemPara$c <- 0
+  }
 
   # replicate item parameter and theta
   itemParaRep <-itemPara[rep(seq_len(nrow(itemPara)), each = length(theta)),]
   itemParaRep$theta <- rep(theta, each = 1, length.out = length(theta)*nrow(itemPara))
 
-  # calculate information by theta
+
+  # # calculate information by theta 2PL
+  # itemParaRep <- within(itemParaRep, {
+  #   P = 0 + (1 - 0) / (1 + exp(-1.702 * a * (theta - b)))
+  #   Q = 1 - P
+  #   PQ = P * Q
+  #   info = 1.702**2 * a**2 * P * Q
+  # })
+
+  # calculate information by theta 3PL
   itemParaRep <- within(itemParaRep, {
-    P = 0 + (1 - 0) / (1 + exp(-1.702 * a * (theta - b)))
+    P = c + (1 - c) / (1 + exp(-1.702 * a * (theta - b)))
     Q = 1 - P
     PQ = P * Q
-    info = 1.702**2 * a**2 * P * Q
+    info = 1.702**2 * a**2 * (Q/P) * (P-c)^2 / (1-c)^2
   })
 
   # sum information by theta
